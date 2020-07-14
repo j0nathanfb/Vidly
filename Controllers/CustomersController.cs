d2420418e9513e351cfbb5a62ac56550bdd2308d
Jonathan Burton <j0nathanfb@yahoo.co.uk>
2020-07-14 15:21:48 +0100

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using Antlr.Runtime.Misc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public CustomersController()
+        {
+            _customers.Add(new Customer { Id = 1, Name = "Fred" });
+            _customers.Add(new Customer { Id = 2, Name = "Barney" });
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _customers.SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        private readonly List<Customer> _customers = new ListStack<Customer>();
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _movies.SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+
+        private readonly List<Movie> _movies = new List<Movie> { new Movie { Id = 1, Name = "Star Wars" }, new Movie { Id = 2, Name = "Close Encounters" } };
+
+    }
+}
